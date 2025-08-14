592e49cca97efb05aa05d892bcdb0d98fc25d77d
Wili Diriba <wilidiribaaj@gmail.com>
2025-08-14 18:31:26 +0300

    row

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Banner from '../../components/Banner/Banner'
+import Rowlist from '../../components/Rows/Rowlist/Rowlist'
 function Home() {
   return (
     <>
-
         <Header />
         <Banner />
+        <Rowlist />
         <Footer />
     </>
   )
